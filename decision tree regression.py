2e35a224e1f559bbe85e65600cafbb9c40e3b200
Quocviet Le <quocvietlet@gmail.com>
2024-08-26 23:59:20 -0500

    predict a 6.5 position

--- decision tree regression.py
+++ decision tree regression.py
@@ -10,10 +10,13 @@ from sklearn.tree import DecisionTreeRegressor
 
 
 # import and load dataset
-dataset = pd.read_csv("Position_Salaries")
+dataset = pd.read_csv("Position_Salaries.csv")
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
 # Training Decision Tree Regression Model on the whole dataset
 regressor = DecisionTreeRegressor(random_state=0)
 regressor.fit(X, y)
+
+# predicting a new result
+regressor.predict([[6.5]])
