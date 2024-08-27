331a35edd98789fcc45b077a5dcdf0da997f5b2f
Quocviet Le <quocvietlet@gmail.com>
2024-08-26 23:57:33 -0500

    trained decision tree model

--- decision tree regression.py
+++ decision tree regression.py
@@ -5,7 +5,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# import datatree regression libraries
+from sklearn.tree import DecisionTreeRegressor
+
+
 # import and load dataset
 dataset = pd.read_csv("Position_Salaries")
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
+
+# Training Decision Tree Regression Model on the whole dataset
+regressor = DecisionTreeRegressor(random_state=0)
+regressor.fit(X, y)
